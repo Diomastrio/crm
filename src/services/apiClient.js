0c5b416e6c8094627c2cc60223fef4e8fe9a874b
diomastrio <zhchypyp@gmail.com>
2024-05-13 16:17:23 -0600

    bit of untested logic

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,8 +1,61 @@
-import supabse, { supabaseUrl } from "./supabase";
+import supabase from "./supabase";
 
-async function insertName() {
-  const {
-    data: { name },
-  } = await supabse.auth.getUser();
-  return name;
+export async function createEditClient(newClient, id) {
+  try {
+    const { data, error } = await supabase
+      .from("cliente")
+      .insert([
+        {
+          nombre: newClient.nombre,
+          email: newClient.email,
+          curp: newClient.curp,
+          num_diplomados: newClient.num_diplomados,
+          diplomados_ter: newClient.diplomados_ter,
+          status: newClient.status,
+        },
+      ])
+      .eq("id", id);
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error creating client:", error);
+    throw error;
+  }
+}
+
+export async function getClients() {
+  try {
+    const { data, error } = await supabase.from("cliente").select();
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error getting clients:", error);
+    throw error;
+  }
+}
+
+export async function deleteClient(id) {
+  try {
+    const { data, error } = await supabase
+      .from("cliente")
+      .delete()
+      .eq("id", id);
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error deleting client:", error);
+    throw error;
+  }
 }
