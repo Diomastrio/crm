379b745aa981af65d1c53ca3f4abd4c03254c0c7
diomastrio <zhchypyp@gmail.com>
2024-06-11 16:24:54 -0600

    package name

--- src/features/pdf/CreatePDF.jsx
+++ src/features/pdf/CreatePDF.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
+import React from "react";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
 import { useCliente } from "../cliente/useSelectCliente";
 import Spinner from "../../ui/Spinner";
 import Empty from "../../ui/Empty";
 import Button from "../../ui/Button";
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
+// import pdfMake  from "pdfmake/build/vfs_fonts";
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
@@ -18,72 +19,94 @@ const ReportButton = () => {
 
     const oneWeekFromNow = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
     const oneWeekThen = new Date(Date.now() - 2 * 24 * 60 * 60 * 1000);
-    let filteredClientes = cliente.filter(      
-      (cliente) => (new Date(cliente.fecha_limite) < oneWeekFromNow )&& new Date(cliente.fecha_limite)> oneWeekThen
+    let filteredClientes = cliente.filter(
+      (cliente) =>
+        new Date(cliente.fecha_limite) < oneWeekFromNow &&
+        new Date(cliente.fecha_limite) > oneWeekThen
     );
 
     var docDefinition = {
       content: [
-        { text: 'Clientes con pronto vencimiento', style: 'header' },
+        { text: "Clientes con pronto vencimiento", style: "header" },
         {
-          style: 'tableExample',
+          style: "tableExample",
           table: {
             headerRows: 1,
-            widths: ['*', '*', '*', '*'],
+            widths: ["*", "*", "*", "*"],
             body: [
-              [{ text: 'Email', style: 'tableHeader' }, { text: 'Nombre', style: 'tableHeader' }, 
-              { text: 'Teléfono', style: 'tableHeader' }, { text: 'Diplomado', style: 'tableHeader' }],
-              ...filteredClientes.map(cliente => [cliente.email, cliente.nombre, cliente.telefono, cliente.disciplina])
-            ]
-          }
+              [
+                { text: "Email", style: "tableHeader" },
+                { text: "Nombre", style: "tableHeader" },
+                { text: "Teléfono", style: "tableHeader" },
+                { text: "Diplomado", style: "tableHeader" },
+              ],
+              ...filteredClientes.map((cliente) => [
+                cliente.email,
+                cliente.nombre,
+                cliente.telefono,
+                cliente.disciplina,
+              ]),
+            ],
+          },
         },
-        { text: 'Correos:', style: 'subheader' },
-        ...filteredClientes.map(cliente => {
+        { text: "Correos:", style: "subheader" },
+        ...filteredClientes.map((cliente) => {
           return { text: cliente.email, margin: [0, 5, 0, 0] };
-        })
+        }),
       ],
       styles: {
         header: {
           fontSize: 18,
-          bold: true
+          bold: true,
         },
         subheader: {
           fontSize: 14,
           bold: true,
-          margin: [0, 15, 0, 5]
+          margin: [0, 15, 0, 5],
         },
         tableExample: {
-          margin: [0, 5, 0, 15]
+          margin: [0, 5, 0, 15],
         },
         tableHeader: {
-          bold: true
-        }
-      }
+          bold: true,
+        },
+      },
     };
 
     var pdfDoc = pdfMake.createPdf(docDefinition);
-    pdfDoc.download('Clientes con VENCIMIENTO.pdf');
-    
+    pdfDoc.download("Clientes con VENCIMIENTO.pdf");
 
-      // Generate the PDF and send the email
-      pdfDoc.getBase64(() => {
-        emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, {
-          to_name: 'CES Centro de Estudios Superiores en Negocios y Humanidades',
-          from_name: 'Storm Chasers',
-          to_email: import.meta.env.VITE_EMAILJS_TO,
-          message: filteredClientes,
-          email: filteredClientes.map(cliente => [cliente.email]),
-        }, import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
-        .then(function(response) {
-          //console.log('Email sent:', response);
-        }, function(error) {
-          //console.log('Failed to send email:', error);
-        });
-      });
+    // Generate the PDF and send the email
+    pdfDoc.getBase64(() => {
+      emailjs
+        .send(
+          import.meta.env.VITE_EMAILJS_SERVICE_ID,
+          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+          {
+            to_name:
+              "CES Centro de Estudios Superiores en Negocios y Humanidades",
+            from_name: "Storm Chasers",
+            to_email: import.meta.env.VITE_EMAILJS_TO,
+            message: filteredClientes,
+            email: filteredClientes.map((cliente) => [cliente.email]),
+          },
+          import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        )
+        .then(
+          function (response) {
+            //console.log('Email sent:', response);
+          },
+          function (error) {
+            //console.log('Failed to send email:', error);
+          }
+        );
+    });
   };
 
   return (
-    <Button  variation={"swapii"} onClick={generatePDF}>Generar Reporte PDF</Button>
+    <Button variation={"swapii"} onClick={generatePDF}>
+      Generar Reporte PDF
+    </Button>
   );
 };
 
